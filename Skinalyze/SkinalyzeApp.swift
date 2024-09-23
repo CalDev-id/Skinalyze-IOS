f408f2aac44b8df1e35a22a3640382a36ba433ca
Heical Chandra <haical31@gmail.com>
2024-09-23 15:52:26 +0700

    Initial commit

---
+++ Skinalyze/SkinalyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinalyzeApp.swift
+//  Skinalyze
+//
+//  Created by Heical Chandra on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SkinalyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
